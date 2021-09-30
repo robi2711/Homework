33e094a25bb1840d8fba2775d54ae97fa27f0a9f
robert <50277245+robi2711@users.noreply.github.com>
2021-09-30 14:28:31 +0100

    Add files via upload

---
+++ Python/Homework for the 30th september.py
@@ -0,0 +1,23 @@
+"""
+P24 Q. 1 to 20
+Bunch of 0s and 1s
+low electricity and high electricity
+american standard code for information interchange
+0 and 1
+8
+every chatacter is assigned a number such as A is 65
+turns a char into its assigned number
+ascii
+chr() char
+scrambles something to make it more secure
+you can add a number to the ascii so it turns into a caesar cipher
+adds a number to the char
+h, i, j
+.upper .lower
+.capitalize
+len() int
+python gives every item in a string an index number
+print specific characters from the string
+0
+14
+"""
