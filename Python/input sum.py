e170b87fc06595e91ab709d02d5125d944e7cc0d
robert <50277245+robi2711@users.noreply.github.com>
2021-10-05 10:18:57 +0100

    Update input sum.py

--- Python/input sum.py
+++ Python/input sum.py
@@ -1,4 +1,14 @@
-sum1 = int(input("First num? "))
-sum2  = int(input("Second num? "))
-sum1 =+ sum2
-print(f"the answer is {sum}")
+import random
+import time
+while True:
+    start_time = time.time()
+    num1 = random.randint(1, 12)
+    num2 = random.randint(1, 12)
+    answer = num1*num2
+    user = int(input(f"What is the sum of {num1} and {num2}? "))
+    if user == answer:
+        print("Thats right!")
+    else:
+        print(f"Thats wrong... \nThe answer is {answer}")
+        elapsed_time = time.time() - start_time
+        print(f"{elapsed_time} seconds taken")
