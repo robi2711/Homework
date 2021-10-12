8898d6b9f46b66e93612615364c6b1be545d24d7
robert <50277245+robi2711@users.noreply.github.com>
2021-10-12 10:15:15 +0100

    Update HW for 11th of october.py

--- HW for 11th of october.py
+++ HW for 11th of october.py
@@ -55,3 +55,59 @@ else:
 
 #Task 4
 
+user = input("Write A/B/C > ")
+if user.upper() == "A":
+    print("You have won a ticket to Dundrum shopping centre")
+elif user.upper() == "B":
+    print("You have won a ticket to Tallaght")
+elif user.upper() == "C":
+    print("You have won a ticket to Broombridge")
+else:
+    print("Invalid input")
+    
+    
+#Task 5
+
+percentage = int(input("What percentage did you get? "))
+if 100 >= percentage >= 90:
+    print("You got a H1")
+elif 89 >= percentage >= 80:
+    print("You got a H2")
+elif 79 >= percentage >= 70:
+    print("You got a H3")
+elif 69 >= percentage >= 60:
+    print("You got a H4")
+elif 59 >= percentage >= 50:
+    print("You got a H5")
+elif 49 >= percentage >= 40:
+    print("You got a H6")
+elif 39 >= percentage >= 30:
+    print("You got a H7")
+elif percentage < 30:
+    print("You got a H8")
+else:
+    print("Sorry i didnt understand that...")
+
+"""
+1. When a program runs over itself multiple times
+2. a loop puts the prgram in a loop
+3. while loop makes program loop while it is not true
+4. Bool
+5. True
+6. :
+7.
+8.
+9. while True:
+10. to loop over something a number of times or infinite times
+11. When a loop is repeated one time too many
+12. add += 1 rin the loop
+13. It ends as soon as you want it to
+"""
+
+#Task 1
+counter = 1000
+if counter >= 1000:
+    x = True
+while counter != 1500 and x:
+    print(counter)
+    counter += 1
