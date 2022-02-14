adf6fdb0b14b0a1a10303dfb4c6c7f68f9490416
robert <robert.gontaru@outlook.com>
2022-02-14 21:54:37 +0000

    finished tasks pg 80

--- Python/pg 79 Tasks/Task3.py
+++ Python/pg 79 Tasks/Task3.py
@@ -9,3 +9,13 @@ while counter != 100:
     randint = random.randint(1, 100)
     m.fa(file, f"{randint},")
     counter += 1
+file2 = "Task3_Impute.csv"
+m.fw(file2, "")
+data = m.fr(file)
+dataList = data.split(",")
+del dataList[-1]
+for item in dataList:
+    if int(item) > 30:
+        m.fa(file2, "30,")
+    else:
+        m.fa(file2,f"{item},")
