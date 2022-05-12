9d6be54b4462470b3145206ad3041bb2725a7445
robert <robert.gontaru@outlook.com>
2022-05-12 20:10:22 +0100

    Update Task 5.py

--- Python/pg 207 Tasks/Task 5.py
+++ Python/pg 207 Tasks/Task 5.py
@@ -1,2 +1,1 @@
-def maxNum(x, y, z):Mlist = [x,y,z];Mlist.sort();return Mlist[-1]
-    # 8,3,4--------------3,4,5
+def maxNum(x, y, z):Mlist = [x,y,z];Mlist.sort();return Mlist[-1]  # 8,3,4--------------3,4,5
