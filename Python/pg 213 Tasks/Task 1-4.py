1ae7d97df6d31ce294b4761d364fc4686ecaf68d
robert <50277245+robi2711@users.noreply.github.com>
2022-05-16 12:14:32 +0100

    Create Task 1-4.py

---
+++ Python/pg 213 Tasks/Task 1-4.py
@@ -0,0 +1,9 @@
+# Task 1
+# look at the first value... if the value is not the key then look at the next value etc.
+# Task 2
+# look at the middle value of the list... if the key is smaller than the value look at the first half of the list... if the key is bigger than the value then look at the second half of the list. etc.
+# Task 3
+# Explained in the questions
+# Task 4
+
+inList = []
