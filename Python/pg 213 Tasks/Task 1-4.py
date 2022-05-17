62eed5f477fadb7d1773ea9ff2ec68094ea9570d
robert <50277245+robi2711@users.noreply.github.com>
2022-05-17 10:02:22 +0100

    Update Task 1-4.py

--- Python/pg 213 Tasks/Task 1-4.py
+++ Python/pg 213 Tasks/Task 1-4.py
@@ -6,4 +6,29 @@
 # Explained in the questions
 # Task 4
 
-inList = []
+inList = [19, 87, 1, -1, 11, 0, -1, 33, 19]
+key = int(input("What is the key"))
+section = input("What part of the question do you want to do?")
+if section == "a":
+  counter = 0
+  while counter != len(inList):
+    if key == inList[counter]:
+      found = True
+      print(f"The key can be found at loc: {counter+1}")
+    counter += 1
+    if found != True:
+      print(f"key:{key} was not found!")
+if section == "d":
+  low = 0
+  high = len(inList) - 1
+  mid = 0
+  while low <= high:
+    mid = (high + low) // 2
+    if inList[mid] < key:
+        low = mid + 1
+    elif inList[mid] > key:
+        high = mid - 1
+    else:
+      answer = mid
+  print(answer)
+      
