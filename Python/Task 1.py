df61d65b84e7c38b7b889ac1e9f8f4806994e17f
robert <50277245+robi2711@users.noreply.github.com>
2022-09-28 10:47:52 +0100

    Update and rename Python/Task 1.py to Python/pg 270/Task 1.py

--- Python/Task 1.py
+++
@@ -1,8 +0,0 @@
-import random
-def dice_roll():
-    return random.randint(1,6)
-user_guess = int(input("Write a random number between 1 and 6 "))
-if user_guess != dice_roll():
-    print("Wrong guess!")
-else:
-    print("Correct")
---
+++ Python/pg 270/Task 1.py
@@ -0,0 +1,31 @@
+def monthlyRepayment(principalAmount, interest, years):
+    finalAmount = principalAmount*((1+interest)**years)
+    monthlyAmount = (finalAmount/ (years*12))
+    return round(monthlyAmount, 2)
+
+def fa(name, write):
+    file = open(name, "a")
+    file.write(write)
+    file.close
+
+def fw(name, write):
+    file = open(name, "w")
+    file.write(write)
+    file.close
+    
+def fr(name):
+    file = open(name, "r")
+    return file.read()
+    file.close
+
+f = "results.csv"
+fw(f, "")
+i = [0.032, 0.043, 0.037, 0.043, 0.044, 0.029, 0.028, 0.030]
+t = [6, 7, 4, 3, 4, 6, 7, 9]
+
+counter = 0
+while len(i) != 0:
+    print(str(monthlyRepayment(10000, i[0], t[0])))
+    fa(f, f"str(monthlyRepayment(10000, i[0], t[0])), ")
+    del i[0]
+    del t[0]
